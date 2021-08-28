803b226e6a67811f797dd3b2ac280832e6321496
Ramesh Vishnoi <rameshvishnoi90904@gmail.com>
2021-08-28 14:51:55 +0530

    feat: Added context api and reusable components

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,32 @@
+import React,{useState, useEffect} from 'react';
+import {
+  View,
+  ActivityIndicator,
+  Dimensions
+} from 'react-native';
+const { width, height } = Dimensions.get('window');
+/**
+ * Component for showing fullscreen loading spinner.
+ *
+ * @component
+ * @example
+ * return (
+ *   <View>
+ *      ...other elements
+ *      <LoadingSpinner/>
+ *   </View>
+ * )
+ */
+function LoadingSpinner(props){
+	const bgColor = props.backgroundColor ? props.backgroundColor : 'rgba(52, 52, 52, 0.4)';
+	const style = props.style ? props.style : {};
+
+	const spinnerColor = props.spinnerColor ? props.spinnerColor :"#00ff00"
+	return (
+		<View style={[{backgroundColor:bgColor, position:'absolute', width: width, height: height, justifyContent: 'center', alignItems:'center'}, style]}>
+			<ActivityIndicator size="large" color={spinnerColor} />
+		</View>
+	);
+}
+
+export default LoadingSpinner;
---
+++ src/context/context.js
@@ -0,0 +1,44 @@
+import React, { useReducer, useMemo } from 'react';
+import PropTypes from 'prop-types';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { sortUsers } from '../helper';
+const initialState = {
+    contactList: [],
+    syncEnabled: false
+};
+
+const initialContext = [{ ...initialState }, () => {}];
+
+export const UserContext = React.createContext(initialContext);
+
+/**
+ * 
+ * @constructor
+ * @param {Object} state - Old State
+ * @param {Object} update - object containing type and payload for updating state
+ * 
+ * @returns {Object} - updated state value
+ */
+const updater = (state, update) => {
+    var toUpdate = {...state};
+    switch (update.type) {
+        
+    }
+    return toUpdate;
+}
+
+export function UserController(props) {
+    const [userState, updateUser] = useReducer(updater, initialState);
+    const value = useMemo(() => [userState, updateUser], [userState]);
+    return (<UserContext.Provider value={value}>
+            {props.children}
+            </UserContext.Provider>);
+  }
+  
+  UserController.propTypes = {
+      children: PropTypes.oneOfType([
+          PropTypes.arrayOf(PropTypes.node),
+          PropTypes.node
+      ]).isRequired
+  };
+  
