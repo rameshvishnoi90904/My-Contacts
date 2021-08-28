71f398285cebabc3bf133a26641e496e6105d45a
Ramesh Vishnoi <rameshvishnoi90904@gmail.com>
2021-08-28 16:25:39 +0530

    feat: Added mockdata for testing purpose

---
+++ src/mockdata/contactList.js
@@ -0,0 +1,62 @@
+export const contactList = [
+    {
+      recordID: '1',
+      givenName: 'Elon',
+      familyName: 'Musk',
+      emailAddresses: [{
+        email: 'andre@gmail.com', 
+        label: 'work'}],
+      phoneNumbers: [{
+        number: '+23493023902',
+        label: 'personal'}],
+      picture: '',
+    },
+    {
+      recordID: '2',
+      givenName: 'Jack',
+      familyName: 'Ma',
+      emailAddresses: [{
+        email: 'andre@gmail.com', 
+        label: 'work'}],
+      phoneNumbers: [{
+        number: '+23493023902',
+        label: 'personal'}],
+      picture: '',
+    },
+    {
+      recordID: '3',
+      givenName: 'Larry',
+      familyName: 'Page',
+      emailAddresses: [{
+        email: 'andre@gmail.com', 
+        label: 'work'}],
+      phoneNumbers: [{
+        number: '+23493023902',
+        label: 'personal'}],
+      picture: '',
+    },
+    {
+      recordID: '4',
+      givenName: 'Mark',
+      familyName: 'Zuck',
+      emailAddresses: [{
+        email: 'andre@gmail.com', 
+        label: 'work'}],
+      phoneNumbers: [{
+        number: '+23493023902',
+        label: 'personal'}],
+      picture: '',
+    },
+    {
+      recordID: '5',
+      givenName: 'Jeff',
+      familyName: 'Bezos',
+      emailAddresses: [{
+        email: 'andre@gmail.com', 
+        label: 'work'}],
+      phoneNumbers: [{
+        number: '+23493023902',
+        label: 'personal'}],
+      picture: '',
+    },
+];
