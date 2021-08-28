0389f1bc9aaf68a56a37134258989a9d3d008459
Ramesh Vishnoi <rameshvishnoi90904@gmail.com>
2021-08-28 16:19:28 +0530

    feat: Added ContactList and Detail Screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,194 @@
-import React from 'react';
-import {Image, View, TouchableOpacity, Text} from 'react-native';
+import React, {useEffect, useState, useContext} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Button,
+  PermissionsAndroid,
+  FlatList,
+  Touchable,
+  TouchableOpacity
+} from 'react-native';
 
-const HomeScreen = () => {
+import Contacts from 'react-native-contacts';
+import {checkMultiple, PERMISSIONS, requestMultiple, request} from 'react-native-permissions';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import { UserContext } from '../context/context';
+import { getUserInitials } from '../helper';
+const colorList = [
+  '#4a5568',
+  '#c53030',
+  '#c05621',
+  '#b7791f',
+  '#2f855a',
+  '#2c7a7b',
+  '#2b6cb0',
+  '#4c51bf',
+  '#6b46c1',
+  '#b83280',
+]
+
+const HomeScreen = ({navigation}) => {
+    const [userState, updateState] = useContext(UserContext);
+    const sync = () => {
+        try {
+          checkMultiple([PERMISSIONS.ANDROID.WRITE_CONTACTS, PERMISSIONS.ANDROID.READ_CONTACTS]).then((result) => {
+            requestMultiple([PERMISSIONS.ANDROID.WRITE_CONTACTS, PERMISSIONS.ANDROID.READ_CONTACTS]).then((res) => {
+              getContacts();
+            });
+          });
+        } catch (error) {
+            console.log("Error :: ",error)
+        }
+    }
+
+    const getContacts = () => {
+      Contacts.getAll().then(contacts => {
+        const stringifiedValue = JSON.stringify(contacts);
+        AsyncStorage.setItem("@contacts",stringifiedValue)
+        updateState({type: "triggerSync"})
+        updateState({type: "init", data: {contactList: contacts}})
+      })
+    }
+
+    const navigateToContactDetail = (data, selectedColor) => {
+      navigation.navigate("Contact Details",{
+        contactInfo: data,
+        selectedColor: selectedColor
+      })
+    }
+    const navigateToEdit = (data) => {
+      navigation.navigate("Edit Contact",{
+        contactInfo: data,
+      })
+    }
+    const renderContactItem = ({item, index}) => {
+      let contactNumber;
+      if (item.phoneNumbers && item.phoneNumbers[0]) {
+        contactNumber = item.phoneNumbers[0].number;
+      }
+      let userInitials = getUserInitials(item);
+      const selectedColor = colorList[index%10];
+      return (
+        <TouchableOpacity
+        accessibilityRole="link"
+        accessibilityHint={`${item.givenName} ${item.familyName} Contact Item`}
+        style={styles.contactItem} 
+        onPress={() => navigateToContactDetail(item, selectedColor)}>
+          <View style={[styles.profilePicture, {backgroundColor: selectedColor}]}>
+            <Text style={styles.userInitials}>{userInitials}</Text>
+          </View>
+          <View style={styles.contactInfo}>
+            <View style={{flex: 1}}>
+              <Text style={styles.contactName}>{`${item.givenName} ${item.familyName}`}</Text>
+              <Text style={styles.contactNumber}>{contactNumber}</Text>
+            </View>
+              <TouchableOpacity onPress={() => navigateToEdit(item)}>
+                <Icon name="account-edit" size={32}/>
+              </TouchableOpacity>
+          </View>
+        </TouchableOpacity>
+      )
+    }
+
+    let syncInfo;
+    if (userState.contactList.length == 0) {
+      syncInfo = (
+        <View style={styles.syncContainer}>
+          <Icon name="database-sync" size={52}/>
+          <Text 
+            accessibilityRole="header"
+            accessibilityAutoFocus
+            style={styles.syncText}>Sync now to import contact from your phone</Text>
+        </View>
+      )
+    }
+
+    let syncButton;
+    if (!userState.syncEnabled || userState.contactList.length == 0) {
+        syncButton = (
+            <Button 
+                testID="sync-btn"
+                title="Sync" 
+                accessible={true}
+                accessibilityLabel="Sync contacts"
+                accessibilityHint="Syncs contacts from device"
+                onPress={sync}/>
+        )
+    }
+
+    let contactList;
+    if (userState.contactList.length > 0) {
+        contactList = (
+            <FlatList
+                  testID="flatlist"
+                  data={userState.contactList}
+                  renderItem={renderContactItem}
+                  keyExtractor={(item) => item.recordID}
+                ></FlatList>
+        )
+    }
+
     return (
-        <View>
+        <View style={styles.container} testID="root">
+            {contactList}
+            {syncInfo}
+            {syncButton}
         </View>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingHorizontal: 16,
+      paddingVertical: 12,
+      backgroundColor: 'white'
+    },
+    contactItem: {
+      marginBottom: 12,
+      flexDirection: 'row',
+    },
+    profilePicture: {
+      width: 60,
+      height: 60,
+      borderRadius: 30,
+      marginRight: 16,
+      backgroundColor: '#8D8D8D',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    contactInfo: {
+      borderBottomWidth: 1,
+      flex: 1,
+      borderBottomColor: '#C9C9C9',
+      flexDirection: 'row',
+      alignItems: 'center'
+    },
+    contactName: {
+      fontSize: 16,
+      color: '#333333'
+    },
+    contactNumber: {
+      color: '#8D8D8D'
+    },
+    userInitials: {
+      color: 'white',
+      fontSize: 22,
+      fontWeight: 'bold'
+    },
+    syncText: {
+      fontSize: 18
+    },
+    syncContainer: {
+      flex: 1, 
+      alignItems: 'center', 
+      justifyContent: 'center'
+    }
+  });
 
 export default HomeScreen;
--- src/screens/PreviewContact.js
+++ src/screens/PreviewContact.js
@@ -1,11 +1,132 @@
 import React from 'react';
-import {Image, View, TouchableOpacity} from 'react-native';
+import {
+    Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { getUserInitials } from '../helper';
+import Icon from 'react-native-vector-icons/FontAwesome5'
+import EIcon from 'react-native-vector-icons/Entypo'
 
-const PreviewContact = () => {
+const PreviewContact = ({navigation, route}) => {
+    const contactInfo = route.params.contactInfo;
+    const selectedColor = route.params.selectedColor;
+    
+    const pList = contactInfo.phoneNumbers.map((item) => {
+        return (
+            <View style={styles.phoneNumberItem} key={item.number}>
+                <View style={{flex: 1}}>
+                    <Text style={styles.numberText}>{item.number}</Text>
+                    <Text style={styles.numberTypeLabel}>{item.label}</Text>
+                </View>
+                <Icon name="phone-alt" size={22} style={{marginHorizontal: 16}}/>
+            </View>
+        )
+    });
+    const eList = contactInfo.emailAddresses.map((item) => {
+        return (
+            <View style={styles.phoneNumberItem} key={item.email}>
+                <View style={{flex:1}}>
+                    <Text style={styles.emailText}>{item.email}</Text>
+                    <Text style={styles.numberTypeLabel}>{item.label}</Text>
+                </View>
+                <EIcon name="email" size={22} style={{marginHorizontal: 16}}/>
+            </View>
+        )
+    });
+
+    const navigateToEdit = () => {
+        navigation.navigate("Edit Contact",{
+            contactInfo: contactInfo
+        });
+    }
     return (
-        <View>
-        </View>
+        <ScrollView style={styles.container}>
+            <View style={styles.profilePictureContainer}>
+                <View style={[styles.profilePicture, {backgroundColor: selectedColor}]}>
+                    <Text style={styles.userInitials}>{getUserInitials(contactInfo)}</Text>
+                </View>
+            </View>
+            <Text 
+                accessibilityRole="header"
+                testID="contact-name"
+                style={styles.contactName}>{contactInfo.givenName} {contactInfo.familyName}</Text>
+            <View style={styles.phoneNumberContainer}>
+                {pList}
+            </View>
+            <View style={styles.phoneNumberContainer}>
+                {eList}
+            </View>
+            <View style={{flex: 1}}></View>
+            <View style={{marginBottom: 16, marginHorizontal: 16}}>
+                <Button 
+                title="Edit"
+                accessibilityLabel="Edit contact"
+                accessibilityHint="Opens Contact detail in new page for editing"
+                onPress={navigateToEdit}/>
+            </View>
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'white'
+    },
+    profilePicture: {
+        width: 120,
+        height: 120, 
+        borderRadius: 60,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'gray'
+    },
+    profilePictureContainer: {
+        alignItems: 'center',
+        marginTop: 32,
+        marginBottom: 16
+    },
+    contactName: {
+        fontSize: 22,
+        textAlign: 'center'
+    },
+    topSection: {
+        paddingBottom: 32
+    },
+    phoneNumberItem: {
+        borderBottomWidth: 1,
+        paddingBottom: 12,
+        paddingTop: 8,
+        borderBottomColor: '#C9C9C9',
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    numberText: {
+        fontSize: 20,
+    },
+    emailText: {
+        fontSize: 16,
+    },
+    phoneNumberContainer: {
+        paddingHorizontal: 16,
+        marginVertical: 8
+    },
+    numberTypeLabel: {
+        textTransform: 'capitalize',
+        color: '#6D6D6D',
+        fontSize: 12
+    },
+    userInitials: {
+        color: 'white',
+        fontSize: 32,
+        fontWeight: 'bold'
+      },
+      sectionHeader: {
+          fontSize: 16
+      }
+  });
 
 export default PreviewContact;
